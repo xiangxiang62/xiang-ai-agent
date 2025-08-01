3b3417baef3ac5a1cb66ffb1091a2126342edb7a
xiangxiang <1964362087@qq.com>
2025-08-01 23:53:14 +0800

    feat：新增健康检查接口

---
+++ src/main/java/com/xiang/xiangaiagent/controller/headCheckController.java
@@ -0,0 +1,28 @@
+package com.xiang.xiangaiagent.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName headCheckController
+ * @Description
+ * @Author xiangxiang
+ * @Date 2025-08-2025/8/1-23:49
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/health")
+public class headCheckController {
+
+
+    /**
+     * 健康检查接口
+     *
+     * @return
+     */
+    @GetMapping("/")
+    public String healthCheck() {
+        return "ok";
+    }
+}
