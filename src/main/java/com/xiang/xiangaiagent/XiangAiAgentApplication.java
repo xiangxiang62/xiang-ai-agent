0df0aaa835156c3190eac34cdc2613dcd1fbecbe
xiangxiang <1964362087@qq.com>
2025-08-01 23:33:22 +0800

    feat: 项目初始化

---
+++ src/main/java/com/xiang/xiangaiagent/XiangAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.xiang.xiangaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiangAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiangAiAgentApplication.class, args);
+    }
+
+}
