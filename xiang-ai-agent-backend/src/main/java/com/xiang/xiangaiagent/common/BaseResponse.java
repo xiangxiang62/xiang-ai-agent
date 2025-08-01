e62dd6b70d5f265a96ded1c57425f19c616a7370
xiangxiang <1964362087@qq.com>
2025-08-02 00:21:06 +0800

    feat：AI 智能体初始化代码

---
+++ xiang-ai-agent-backend/src/main/java/com/xiang/xiangaiagent/common/BaseResponse.java
@@ -0,0 +1,36 @@
+package com.xiang.xiangaiagent.common;
+
+import com.xiang.xiangaiagent.exception.ErrorCode;
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+/**
+ * 统一相应类
+ *
+ * @param <T>
+ */
+@Data
+public class BaseResponse<T> implements Serializable {
+
+    private int code;
+
+    private T data;
+
+    private String message;
+
+    public BaseResponse(int code, T data, String message) {
+        this.code = code;
+        this.data = data;
+        this.message = message;
+    }
+
+    public BaseResponse(int code, T data) {
+        this(code, data, "");
+    }
+
+    public BaseResponse(ErrorCode errorCode) {
+        this(errorCode.getCode(), null, errorCode.getMessage());
+    }
+}
---
+++ xiang-ai-agent-backend/src/main/java/com/xiang/xiangaiagent/common/DeleteRequest.java
@@ -0,0 +1,19 @@
+package com.xiang.xiangaiagent.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 删除请求类
+ */
+@Data
+public class DeleteRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
