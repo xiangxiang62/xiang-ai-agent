1b9eeca35573d379cb715b01fea8831516077cd7
xiangxiang <1964362087@qq.com>
2025-08-02 23:46:23 +0800

    feat: 引入 Antd Vue

--- xiang-ai-agent-frontend/src/App.vue
+++ xiang-ai-agent-frontend/src/App.vue
@@ -6,6 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <a-button type="primary">Primary Button</a-button>
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
