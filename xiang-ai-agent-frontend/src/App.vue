ab549e8d7de9d97760b516113dfc822ccb13858b
xiangxiang <1964362087@qq.com>
2025-08-04 00:24:30 +0800

    feat：前端整合用户模块

--- xiang-ai-agent-frontend/src/App.vue
+++ xiang-ai-agent-frontend/src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import BasicLayout from '@/layouts/BasicLayout.vue'
-
+import { useLoginUserStore } from '@/stores/loginUser.ts'
 import { healthCheck } from '@/api/headCheckController.ts'
 
 healthCheck().then((res) => {
   console.log(res)
 })
+
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
 </script>
 
 <template>
