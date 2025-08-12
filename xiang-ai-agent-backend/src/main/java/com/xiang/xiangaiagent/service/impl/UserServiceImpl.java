1b227a04cbcab70b4a4391391016cabd8c516f26
xiangxiang <1964362087@qq.com>
2025-08-13 01:40:59 +0800

    feat：使用设计模式将代码重构 - 执行器模式（提供统一的执行入口，根据生成类型完成不同操作） - 策略模式（每种模式对应的解析方法抽象为一个类来进行维护） - 模板方法模式（抽象模板类定义了通用的文件保存流程，子类可以进行自己的实现，比如多文件生成需要保存 3 个文件，而 HTML 模式只需要 1 个文件）

--- xiang-ai-agent-backend/src/main/java/com/xiang/xiangaiagent/service/impl/UserServiceImpl.java
+++ xiang-ai-agent-backend/src/main/java/com/xiang/xiangaiagent/service/impl/UserServiceImpl.java
@@ -86,7 +86,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>  implements U
     @Override
     public String getEncryptPassword(String userPassword) {
         // 盐值，混淆密码
-        final String SALT = "yupi";
+        final String SALT = "xiangxiang";
         return DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
     }
 
