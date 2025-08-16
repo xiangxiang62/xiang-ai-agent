4010cec369262e50c2f69fe4343f570f0d96fe7a
xiangxiang <1964362087@qq.com>
2025-08-16 17:38:45 +0800

    feat：AI 生成代码流式输出功能

--- xiang-ai-agent-backend/src/main/java/com/xiang/xiangaiagent/service/AppService.java
+++ xiang-ai-agent-backend/src/main/java/com/xiang/xiangaiagent/service/AppService.java
@@ -2,9 +2,17 @@ package com.xiang.xiangaiagent.service;
 
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+import com.xiang.xiangaiagent.core.AiCodeGeneratorFacade;
+import com.xiang.xiangaiagent.exception.BusinessException;
+import com.xiang.xiangaiagent.exception.ErrorCode;
+import com.xiang.xiangaiagent.exception.ThrowUtils;
 import com.xiang.xiangaiagent.model.dto.app.AppQueryRequest;
 import com.xiang.xiangaiagent.model.entity.App;
+import com.xiang.xiangaiagent.model.entity.User;
+import com.xiang.xiangaiagent.model.enums.CodeGenTypeEnum;
 import com.xiang.xiangaiagent.model.vo.AppVO;
+import jakarta.annotation.Resource;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -15,6 +23,16 @@ import java.util.List;
  */
 public interface AppService extends IService<App> {
 
+
+    /**
+     * 对话生成代码
+     *
+     * @param appId 应用 id
+     * @param message 用户提示词
+     * @param loginUser 登录用户
+     * @return
+     */
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
 
     /**
      * 批量获取应用返回类
--- xiang-ai-agent-backend/src/main/java/com/xiang/xiangaiagent/service/impl/AppServiceImpl.java
+++ xiang-ai-agent-backend/src/main/java/com/xiang/xiangaiagent/service/impl/AppServiceImpl.java
@@ -2,20 +2,25 @@ package com.xiang.xiangaiagent.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
+import com.xiang.xiangaiagent.core.AiCodeGeneratorFacade;
 import com.xiang.xiangaiagent.exception.BusinessException;
 import com.xiang.xiangaiagent.exception.ErrorCode;
+import com.xiang.xiangaiagent.exception.ThrowUtils;
 import com.xiang.xiangaiagent.model.dto.app.AppQueryRequest;
 import com.xiang.xiangaiagent.model.entity.App;
 import com.xiang.xiangaiagent.mapper.AppMapper;
 import com.xiang.xiangaiagent.model.entity.User;
+import com.xiang.xiangaiagent.model.enums.CodeGenTypeEnum;
 import com.xiang.xiangaiagent.model.vo.AppVO;
 import com.xiang.xiangaiagent.model.vo.UserVO;
 import com.xiang.xiangaiagent.service.AppService;
 import com.xiang.xiangaiagent.service.UserService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +39,40 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
     @Resource
     private UserService userService;
 
+
+    @Resource
+    private AiCodeGeneratorFacade aiCodeGeneratorFacade;
+
+
+    /**
+     * 对话生成代码
+     *
+     * @param appId 应用 id
+     * @param message 用户提示词
+     * @param loginUser 登录用户
+     * @return
+     */
+    @Override
+    public Flux<String> chatToGenCode(Long appId, String message, User loginUser) {
+        // 1. 参数校验
+        ThrowUtils.throwIf(appId == null || appId <= 0, ErrorCode.PARAMS_ERROR, "应用 ID 不能为空");
+        ThrowUtils.throwIf(StrUtil.isBlank(message), ErrorCode.PARAMS_ERROR, "用户消息不能为空");
+        // 2. 查询应用信息
+        App app = this.getById(appId);
+        ThrowUtils.throwIf(app == null, ErrorCode.NOT_FOUND_ERROR, "应用不存在");
+        // 3. 验证用户是否有权限访问该应用，仅本人可以生成代码
+        if (!app.getUserId().equals(loginUser.getId())) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR, "无权限访问该应用");
+        }
+        // 4. 获取应用的代码生成类型
+        String codeGenTypeStr = app.getCodeGenType();
+        CodeGenTypeEnum codeGenTypeEnum = CodeGenTypeEnum.getEnumByValue(codeGenTypeStr);
+        if (codeGenTypeEnum == null) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "不支持的代码生成类型");
+        }
+        // 5. 调用 AI 生成代码
+        return aiCodeGeneratorFacade.generateAndSaveCodeStream(message, codeGenTypeEnum, appId);
+    }
 
     /**
      * 批量获取应用返回类
